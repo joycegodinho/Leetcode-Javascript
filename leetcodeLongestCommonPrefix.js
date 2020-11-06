c209ac08d972ea29f240b4ce90a0f6e31be3b3ac
Joyce Bosco <joycegodinhobosco@gmail.com>
2020-11-05 21:34:39 -0300

    first commit

---
+++ leetcodeLongestCommonPrefix.js
@@ -0,0 +1,29 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+    
+    if (strs.length == 0){
+        return ""
+    }
+    
+    strs.sort();
+    var first = strs[0];
+    var last = strs[strs.length - 1];
+    var resp = "";
+    
+    for(var i = 0; i<= first.length; i++ ){
+        
+        var subFirst = first.substring(i-1 ,i);
+        var subLast = last.substring(i-1 ,i);
+        
+        if(subFirst == subLast){
+            resp = resp + first.substring(i-1 ,i);
+        }
+        else{
+            break;
+        }
+    }
+    return resp;
+};
