40dbea7c6ed2001d455c06a9baffc89af1ac6540
Joyce Bosco <joycegodinhobosco@gmail.com>
2020-11-05 21:35:32 -0300

    first commit

---
+++ leetcodeRemoveDuplicates.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+
+    var i = 0;
+    if (nums.length == 0) {
+        return 0;
+    }
+
+    for (var j = 1; j < nums.length; j++) {
+        if (nums[j] != nums[i]) {
+            i = i + 1;
+            nums[i] = nums[j];
+        }
+    }
+
+    return i + 1;
+
+};
